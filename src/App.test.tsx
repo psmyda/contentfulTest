cb05aabf32236dc11d8cc2b77c8a44db5e33b94c
psmyda <psmyda@gmail.com>
2023-06-17 07:04:16 +0200

    Add simple tests for main component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import mockApi from './services/contentful'
 
-test('renders learn react link', () => {
+const apiResponseMock = {
+      "internalName": "FAQ",
+      "title": "FAQ Test",
+      "accordionItems": [
+          {
+              "sys": {
+                  "id": "test1",
+              },
+              "fields": {
+                  "internalName": "Test1?",
+                  "name": "Test1?",
+                  "text": "Test1Text"
+              }
+          },
+          {
+              "sys": {
+                  "id": "test2",
+              },
+              "fields": {
+                  "internalName": "Test2?",
+                  "name": "Test2?",
+                  "text": "Test2Text"
+              }
+          },
+          {
+              "sys": {
+                  "id": "test3",
+              },
+              "fields": {
+                  "internalName": "Test3?",
+                  "name": "Test3?",
+                  "text": "Test3Text"
+              }
+          }
+      ]
+    };
+
+test('renders home page without api call', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+    expect(screen.getByRole("paragraph")).toHaveClass("placeholder-wave");
+
+});
+
+test('renders home page with api call', async () => {
+    mockApi.getEntries = jest.fn().mockResolvedValue(apiResponseMock);
+    render(<App />);
+
+      expect(await screen.findByText("FAQ Test")).toBeInTheDocument();
+      expect(await screen.findByText("Test1?")).toBeInTheDocument();
+      expect(await screen.findByText("Test2?")).toBeInTheDocument();
+      expect(await screen.findByText("Test3?")).toBeInTheDocument();
 });
